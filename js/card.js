fdc4c8cde23527d853b37d993218036876ace09b
Scott Greenlay <sgreenlay@gmail.com>
2013-01-09 01:36:26 -0500

    Fixed for remote serving of cards.

--- js/card.js
+++ js/card.js
@@ -10,60 +10,57 @@ function params(name) {
 	return results[1];
 }
 
-function defaultCardState() {
-	var card_img = document.getElementById('card-front-img');
-	card_img.src = 'img/error.jpg'
-	
-	var card_txt = document.getElementById('card-back-txt');
-	card_txt.innerHTML = "Hey!<br/>Unfortunately you do not have a valid URL, but thanks for checking this out!</br>- Scott";
-	
-	var card_stamp = document.getElementById('card-stamp');
-	card_stamp.src = 'img/error_stamp.jpg';
+function setCardState(json) {
+	if (json.img) {
+		var card_img = document.getElementById('card-front-img');
+		card_img.src = json.img;
+	}
+	if (json.text) {
+		var card_txt = document.getElementById('card-back-txt');
+		var text = json.text.replace(/\r/g, '<br/>');
+		card_txt.innerHTML = text;
+	}
+	if (json.stamp) {
+		var card_stamp = document.getElementById('card-stamp');
+		card_stamp.src = json.stamp;
+	}
+	if (json.location) {
+		var mapOptions = {
+		  center: new google.maps.LatLng(json.location[0], json.location[1]),
+		  zoom: 7,
+		  mapTypeId: google.maps.MapTypeId.TERRAIN,
+		  disableDefaultUI: true
+		};
+		var map = new google.maps.Map(document.getElementById("map"),
+			mapOptions);
+		
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(json.location[0], json.location[1])
+		});
+		marker.setMap(map);
+	}
+}
+
+function setToDefaultCardState() {
+	var card = {
+		img : 'img/error.jpg',
+		text : "Hey!\rUnfortunately you do not have a valid URL, but thanks for checking this out!\r- Scott",
+		stamp : 'img/error_stamp.jpg'
+	}
+	setCardState(card);
 }
 
 var cardID = params('card');
-
 if (cardID != null) {
-	var req = new XMLHttpRequest();
-	req.open("GET", "http://data.greenlay.net/Cards/" + cardID + '.json', true);
-	req.onreadystatechange = function () {
-		if (req.readyState == 4) {
-			if (req.status == 200) {
-				var json = eval('(' + req.responseText + ')');
-				
-				var card_img = document.getElementById('card-front-img');
-				card_img.src = json.img;
-				
-				var card_txt = document.getElementById('card-back-txt');
-				var text = json.text.replace(/\r/g, '<br/>');
-				card_txt.innerHTML = text;
-				
-				var card_stamp = document.getElementById('card-stamp');
-				card_stamp.src = json.stamp;
-				
-				var mapOptions = {
-				  center: new google.maps.LatLng(json.location[0], json.location[1]),
-				  zoom: 7,
-				  mapTypeId: google.maps.MapTypeId.TERRAIN,
-				  disableDefaultUI: true
-				};
-				var map = new google.maps.Map(document.getElementById("map"),
-					mapOptions);
-				
-				var marker = new google.maps.Marker({
-					position: new google.maps.LatLng(json.location[0], json.location[1])
-				});
-				marker.setMap(map);
-			}
-			else {
-				defaultCardState();
-			}
-		}
-	};
-	req.send(null);
+	var script = document.createElement('script');
+	script.src = 'http://data.greenlay.net/Cards/' + cardID + '.json';
+	script.onerror = function () {
+	   setToDefaultCardState();
+	}
+	document.body.appendChild(script);
 }
 else {
-	defaultCardState();
+	setToDefaultCardState();
 }
 
 function flipCard() {
